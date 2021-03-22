95d7c4b97bc381f59c5717ee1d8c5786e866b1d0
Gustavo Miguel <gustavo.biscaro@gmail.com>
2021-03-22 11:46:51 -0300

    Formatting the HTML and CSS with Materialize

---
+++ galeria.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Clorocine</title>
+</head>
+<body>
+<nav class="nav-extended">
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo">CLOROCINE</a>
+      <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="galeria.php">Galeria</a></li>
+        <li><a href="cadastrar.php">Cadastrar</a></li>
+      </ul>
+    </div>
+    <div class="nav-content">
+      <ul class="tabs tabs-transparent">
+        <li class="tab"><a class="active" href="#test1">Todos</a></li>
+        <li class="tab"><a href="#test2">Assistidos</a></li>
+        <li class="tab disabled"><a href="#test3">Favoritos</a></li>
+      </ul>
+    </div>
+  </nav>
+</body>
+</html>
