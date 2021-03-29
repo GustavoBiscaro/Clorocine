29529c95afd49167b76e7e3cb4ed57b411a9f517
Gustavo Miguel <gustavo.biscaro@gmail.com>
2021-03-29 10:03:38 -0300

    Dynamic with php

--- galeria.php
+++ galeria.php
@@ -1,4 +1,26 @@
 <?php include "cabecalho.php" ?>
+
+<?php
+$filme1 = [
+  "titulo"=>"Vingadores: Ultimato", 
+  "nota" => 8.6,
+  "sinopse" => "Após os eventos devastadores de 'Vingadores: Guerra Infinita', o universo está em ruínas devido aos esforços do Titã Louco, Thanos. Com a ajuda de aliados remanescentes, os Vingadores devem se reunir mais uma vez a fim de desfazer as ações de Thanos e restaurar a ordem no universo de uma vez por todas, não importando as consequências.",
+  "poster" => "https://www.themoviedb.org/t/p/original/q6725aR8Zs4IwGMXzZT8aC8lh41.jpg"
+];
+
+$filme2 = [
+  "titulo"=>"Liga Da Justiça de Zack Snyder", 
+  "nota" => 8.6,
+  "sinopse" => "Determinado a garantir que o sacrifício final do Superman não foi em vão, Bruce Wayne alinha forças com Diana Prince com planos de recrutar uma equipe de metahumanos para proteger o mundo de uma ameaça de proporções catastróficas que se aproxima.",
+  "poster" => "https://www.themoviedb.org/t/p/w300/v9XwEXYWpxt2rpkmFBiQ1zeyKUy.jpg"
+];
+
+
+$filmes = [$filme1, $filme2];
+
+
+
+?>
 <body>
 <nav class="nav-extended purple lighten-3">
     <div class="nav-wrapper"> 
@@ -20,42 +42,26 @@
   </nav>
 
   <div class="row">
+    <?php 
+      foreach($filmes as $filme) :
+      ?>
     <div class="col s3">
       <div class="card hoverable">
     <div class="card-image">
-      <img src="https://www.themoviedb.org/t/p/w300/q6725aR8Zs4IwGMXzZT8aC8lh41.jpg">
+      <img src="<?= $filme["poster"] ?>">
     
       <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">favorite_border</i></a>
     </div>
     <div class="card-content">
-       <p class="valign-wrapper"><i class="material-icons amber-text">star</i> 9,7</p>
-       <span class="card-title">Vingadores: O Ultimato</span>
-      <p>Após os eventos devastadores de "Vingadores: Guerra Infinita", o universo está em ruínas devido aos esforços do Titã Louco, Thanos. Com a ajuda de aliados remanescentes, os Vingadores devem se reunir mais uma vez a fim de desfazer as ações de Thanos e restaurar a ordem no universo de uma vez por todas, não importando as consequências.
-</p>
+       <p class="valign-wrapper">
+         <i class="material-icons amber-text">star</i> <?= $filme["nota"] ?>
+         </p>
+       <span class="card-title"><?= $filme["titulo"] ?></span>
+      <p><?= $filme["sinopse"] ?> </p>
     </div>
   </div>
-    </div>
-    <div class="col s3">
-      <div class="card hoverable">
-    <div class="card-image">
-      <img src="https://www.themoviedb.org/t/p/w300/v9XwEXYWpxt2rpkmFBiQ1zeyKUy.jpg">
-      <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">favorite_border</i></a>
-    </div>
-    <div class="card-content">
-       <p class="valign-wrapper"><i class="material-icons amber-text">star</i> 9,7</p>
-       <span class="card-title">Liga Da Justiça de Zack Snyder</span>
-    <p>Determinado a garantir que o sacrifício final do Superman não foi em vão, Bruce Wayne alinha forças com Diana Prince com planos de recrutar uma equipe de metahumanos para proteger o mundo de uma ameaça de proporções catastróficas que se aproxima.</p>
-    </div>
-  </div>
-    </div>
-  </div>
-
-  
-
-
-
-
-
+</div>
+<?php endforeach ?>
 
 </body>
 </html>
